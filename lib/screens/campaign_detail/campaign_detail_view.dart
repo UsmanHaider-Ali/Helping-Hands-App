99d5f991b2daf9d6dc8a250c4ac6393131c089df
Usman Haider Ali <UsmanHaider_Ali@outlook.com>
2022-11-28 15:55:38 +0500

    Campaign screen design completed.

--- lib/screens/campaign_detail/campaign_detail_view.dart
+++ lib/screens/campaign_detail/campaign_detail_view.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:helping_hands_app/resources/colors_manager.dart';
+import 'package:helping_hands_app/screens/campaign_detail/action_plan_view.dart';
+import 'package:helping_hands_app/screens/campaign_detail/overview_view.dart';
+import 'package:helping_hands_app/screens/campaign_detail/risk_challenges_view.dart';
+import 'package:helping_hands_app/screens/campaign_detail/story_view.dart';
+
+import '../../resources/dimens_manager.dart';
+import '../../widgets/category_title_style.dart';
 
 class CampaignDetailView extends StatefulWidget {
   const CampaignDetailView({super.key});
@@ -9,26 +16,214 @@ class CampaignDetailView extends StatefulWidget {
 }
 
 class _CampaignDetailViewState extends State<CampaignDetailView> {
+  late final List<String> _list = [
+    'https://www.who.int/images/default-source/health-and-climate-change/rescue-operation-haiti-flood-c-un-photo-marco-dormino.tmb-1024v.jpg?sfvrsn=412ab270_1',
+    'https://ichef.bbci.co.uk/news/976/cpsprodpb/76E8/production/_126504403_gettyimages-1242726142_kids976.jpg',
+    'https://images.unsplash.com/photo-1547683905-f686c993aae5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Zmxvb2R8ZW58MHx8MHx8&w=1000&q=80',
+  ];
+
+  PageController _pageController = PageController(
+    initialPage: 0,
+  );
+
+  int _selectedIndex = 0;
+  int imageSelectedAddress = 0;
+
+  void _onCategoryClick(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<String> menuList = [
+    "Overview",
+    "Story",
+    "Action Plan",
+    "Risk and Challenges",
+  ];
+  final List<Widget> menuScreen = [
+    OverView(),
+    StoryView(),
+    ActionPlanView(),
+    RiskChallengesView(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Campaign Detail",
+          "Campaign Details",
           style: Theme.of(context).textTheme.titleMedium,
         ),
         iconTheme: IconThemeData(
           color: ColorsManager.primaryTextColor,
         ),
       ),
-      body: Column(
-        children: [
-          Card(
-            color: Colors.white,
-            child: Container(),
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: 200,
+              child: Stack(
+                children: [
+                  PageView.builder(
+                      controller: _pageController,
+                      itemCount: _list.length,
+                      onPageChanged: (index) {
+                        setState(() {
+                          imageSelectedAddress = index;
+                        });
+                      },
+                      itemBuilder: (context, index) {
+                        return OnBoardingPage(
+                          _list[index],
+                        );
+                      }),
+                  Container(
+                    alignment: Alignment.bottomCenter,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        for (int i = 0; i < _list.length; i++)
+                          _getIndicator(i, imageSelectedAddress),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    child: Card(
+                      color: ColorsManager.lightTextColor.withOpacity(0.7),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Text(
+                          "Emergency",
+                          style: Theme.of(context).textTheme.labelMedium,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    alignment: Alignment.topRight,
+                    child: SizedBox(
+                      height: 40,
+                      width: 90,
+                      child: Card(
+                        color: ColorsManager.lightTextColor.withOpacity(0.7),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            Container(
+                              alignment: Alignment.center,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.chat,
+                                ),
+                                SizedBox(
+                                  width: 12,
+                                ),
+                                Icon(
+                                  Icons.favorite,
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 36.0,
+              child: ListView.builder(
+                physics: ClampingScrollPhysics(),
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: menuList.length,
+                itemBuilder: (BuildContext context, int index) => InkWell(
+                  child: CategoryStyle(
+                    textStyle: _selectedIndex == index
+                        ? TextStyle(
+                            color: ColorsManager.primaryColor,
+                            fontWeight: FontWeight.w500,
+                            decoration: TextDecoration.underline,
+                          )
+                        : TextStyle(
+                            color: ColorsManager.lightTextColor,
+                          ),
+                    title: menuList[index],
+                  ),
+                  onTap: () {
+                    _onCategoryClick(index);
+                  },
+                ),
+              ),
+            ),
+            Expanded(
+              child: menuScreen[_selectedIndex],
+            ),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+Widget _getIndicator(index, imageSelectedAddress) {
+  if (index == imageSelectedAddress) {
+    return _makeIndicator(true);
+  } else {
+    return _makeIndicator(false);
+  }
+}
+
+Widget _makeIndicator(bool isSelected) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(
+      horizontal: 4,
+      vertical: 12,
+    ),
+    child: Container(
+      width: isSelected ? 24 : 8,
+      height: 8,
+      decoration: BoxDecoration(
+        color: ColorsManager.primaryColor,
+        borderRadius: BorderRadius.circular(
+          ValuesManager.defaultRadious,
+        ),
+      ),
+    ),
+  );
+}
+
+class OnBoardingPage extends StatelessWidget {
+  String imageAddress;
+
+  OnBoardingPage(this.imageAddress, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      imageAddress,
+      width: MediaQuery.of(context).size.width,
+      fit: BoxFit.cover,
     );
   }
 }
---
+++ lib/screens/campaign_detail/overview_view.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../resources/assets_manager.dart';
+import '../../resources/colors_manager.dart';
+import '../../resources/dimens_manager.dart';
+
+class OverView extends StatefulWidget {
+  const OverView({Key? key}) : super(key: key);
+
+  @override
+  State<OverView> createState() => _OverViewState();
+}
+
+class _OverViewState extends State<OverView> {
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double widthOfBoxBar = width * 0.6;
+    double widthOfCompletedBar = width * 0.5;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Fund Raising for Flood",
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        SizedBox(
+          height: 4,
+        ),
+        Row(
+          children: [
+            Image.asset(
+              ImageAssetsManager.appLogo,
+              height: 48,
+            ),
+            SizedBox(
+              width: 16,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Created By",
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+                Text(
+                  "Ali Haider",
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+              ],
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Container(
+          width: width,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Raised 1.5 ETH",
+                    style: Theme.of(context).textTheme.titleSmall,
+                  ),
+                  Text(
+                    "Goal 3.0 ETH",
+                    style: Theme.of(context).textTheme.titleSmall,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 4,
+              ),
+              Stack(
+                alignment: Alignment.centerLeft,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: ColorsManager.lightTextColor,
+                      borderRadius: BorderRadius.circular(
+                        ValuesManager.defaultRadious,
+                      ),
+                    ),
+                    height: 16,
+                    width: width,
+                  ),
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: ColorsManager.primaryColor,
+                          borderRadius: BorderRadius.circular(
+                            ValuesManager.defaultRadious,
+                          ),
+                        ),
+                        height: 16,
+                        width: widthOfCompletedBar,
+                      ),
+                      const Text(
+                        "50%",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 10,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Text(
+          "Description",
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        Text(
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+          style: Theme.of(context).textTheme.bodyMedium,
+          textAlign: TextAlign.justify,
+        ),
+      ],
+    );
+  }
+}
